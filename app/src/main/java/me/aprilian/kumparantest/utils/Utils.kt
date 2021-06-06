ead81a9495d8363761c1c2e3979ae6993b66bc56
aprealian <apri12041995@gmail.com>
2021-06-06 13:03:51 +0700

    Enhance some UI and use safeargs to send params to another fragment

--- app/src/main/java/me/aprilian/kumparantest/utils/Utils.kt
+++ app/src/main/java/me/aprilian/kumparantest/utils/Utils.kt
@@ -7,4 +7,12 @@ object Utils {
     fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT){
         Toast.makeText(this, message, duration).show()
     }
+
+    fun pxToDp(context: Context, px: Float): Float {
+        return px / context.resources.displayMetrics.density
+    }
+
+    fun dpToPx(context: Context, dp: Float): Float {
+        return dp * context.resources.displayMetrics.density
+    }
 }
